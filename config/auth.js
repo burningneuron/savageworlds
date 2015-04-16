554b824dc6632e23d088d0b00d9b24b8845011a4
Tom Cannon <tomcannon@MacBook-Pro.local>
2015-04-16 18:26:01 -0500

    fixed facebook callback

--- config/auth.js
+++ config/auth.js
@@ -3,22 +3,22 @@
 // expose our config directly to our application using module.exports
 module.exports = {
 
-	'facebookAuth': {
-		'clientID': process.env.FACEBOOK_CLIENTID, // your App ID
-		'clientSecret': process.env.FACEBOOK_CLIENTSECRET, // your App Secret
-		'callbackURL': 'http://enigmatic-depths-2598.herokuapp.com/auth/facebook/callback'
-	},
+  'facebookAuth': {
+    'clientID': process.env.FACEBOOK_CLIENTID, // your App ID
+    'clientSecret': process.env.FACEBOOK_CLIENTSECRET, // your App Secret
+    'callbackURL': process.env.BASE_URL + process.env.FACEBOOK_CALLBACK
+  },
 
-	'twitterAuth': {
-		'consumerKey': 'your-consumer-key-here',
-		'consumerSecret': 'your-client-secret-here',
-		'callbackURL': 'http://localhost:8080/auth/twitter/callback'
-	},
+  'twitterAuth': {
+    'consumerKey': 'your-consumer-key-here',
+    'consumerSecret': 'your-client-secret-here',
+    'callbackURL': 'http://localhost:8080/auth/twitter/callback'
+  },
 
-	'googleAuth': {
-		'clientID': 'your-secret-clientID-here',
-		'clientSecret': 'your-client-secret-here',
-		'callbackURL': 'http://localhost:8080/auth/google/callback'
-	}
+  'googleAuth': {
+    'clientID': 'your-secret-clientID-here',
+    'clientSecret': 'your-client-secret-here',
+    'callbackURL': 'http://localhost:8080/auth/google/callback'
+  }
 
 };

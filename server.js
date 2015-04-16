e82a683143254d716c024f948a0de749a929b17e
Tom Cannon <tomcannon@MacBook-Pro.local>
2015-04-16 07:55:37 -0500

    still experimenting

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Load required packages
 var express = require('express');
 var mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var session = require('express-session');
@@ -14,7 +15,6 @@ var userController = require('./controllers/user');
 var authController = require('./controllers/auth');
 var oauth2Controller = require('./controllers/oauth2');
 var clientController = require('./controllers/client');
-var FacebookStrategy = require('passport-facebook').Strategy;
 
 var env = require('./config/env.js');
 
@@ -35,7 +35,11 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('port', (env.port || 5000));
 
+// use cookie-parser
+app.use(cookieParser()); // read cookies (needed for auth)
+
 // Use the body-parser package in our application
+app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -79,98 +83,6 @@ router.route('/api/users')
   .get(authController.isAuthenticated, userController.getUsers);
 
 // facebook -------------------------------
-// =========================================================================
-// FACEBOOK ================================================================
-// =========================================================================
-passport.use(new FacebookStrategy({
-
-    clientID: env.facebookClientID,
-    clientSecret: env.facebookClientSecret,
-    callbackURL: env.facebookCallbackUrl,
-    passReqToCallback: true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
-
-  },
-  function(req, token, refreshToken, profile, done) {
-    logger.info('req: ' + req);
-    logger.info('token: ' + token);
-    logger.info('refreshToken: ' + refreshToken);
-    logger.info('profile: ' + profile);
-
-    // asynchronous
-    process.nextTick(function() {
-
-      // check if the user is already logged in
-      if (!req.user) {
-
-        User.findOne({
-          'facebook.id': profile.id
-        }, function(err, user) {
-          if (err)
-            return done(err);
-
-          if (user) {
-
-            // if there is a user id already but no token (user was linked at one point and then removed)
-            if (!user.facebook.token) {
-              user.facebook.token = token;
-              user.facebook.name = profile.name.givenName + ' ' +
-                profile.name
-                .familyName;
-              user.facebook.email = (profile.emails[0].value || '')
-                .toLowerCase();
-
-              user.save(function(err) {
-                if (err)
-                  return done(err);
-
-                return done(null, user);
-              });
-            }
-
-            return done(null, user); // user found, return that user
-          } else {
-            // if there is no user, create them
-            var newUser = new User();
-
-            newUser.facebook.id = profile.id;
-            newUser.facebook.token = token;
-            newUser.facebook.name = profile.name.givenName + ' ' +
-              profile.name
-              .familyName;
-            newUser.facebook.email = (profile.emails[0].value || '')
-              .toLowerCase();
-
-            newUser.save(function(err) {
-              if (err)
-                return done(err);
-
-              return done(null, newUser);
-            });
-          }
-        });
-
-      } else {
-        // user already exists and is logged in, we have to link accounts
-        var user = req.user; // pull the user out of the session
-
-        user.facebook.id = profile.id;
-        user.facebook.token = token;
-        user.facebook.name = profile.name.givenName + ' ' + profile.name
-          .familyName;
-        user.facebook.email = (profile.emails[0].value || '').toLowerCase();
-
-        user.save(function(err) {
-          if (err)
-            return done(err);
-
-          return done(null, user);
-        });
-
-      }
-    });
-
-  }));
-
 // send to facebook to do the authentication
 app.get('/api/auth/facebook', passport.authenticate('facebook', {
   scope: 'email'

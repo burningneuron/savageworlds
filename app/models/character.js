8609c3f6164c70845a5a19ff782d00e488b0a76c
Tom Cannon <tomcannon@MacBook-Pro.local>
2015-04-17 18:53:11 -0500

    first cut at character routes

--- app/models/character.js
+++ app/models/character.js
@@ -3,77 +3,10 @@ var mongoose = require('mongoose');
 
 // Define our character schema
 var CharacterSchema = new mongoose.Schema({
-  userId: String,
-  name: String,
-  setting: String,
-  race: String,
-  rank: String,
-  xps: Number,
-  money: String,
-  baseStats: {
-    agility: String,
-    smarts: String,
-    spirit: String,
-    strength: String,
-    vigor: String,
-    charisma: String,
-    charismaModifier: String,
-    pace: String,
-    sprintDie: String,
-    parry: String,
-    parryModifier: String,
-    toughness: String,
-    armor: String,
-  },
-  otherStats: [{
-    name: String,
-    value: String
-  }],
-  armor: {
-    head: String,
-    torso: String,
-    arms: String,
-    legs: String
-  },
-  encumbrance: {
-    carried: Number,
-    limit: Number,
-    penalty: Number
-  },
-  skills: [{
-    name: String,
-    value: String
-  }],
-  edges: [{
-    name: String,
-    description: String
-  }],
-  hindrances: [{
-    name: String,
-    description: String
-  }],
-  powers: [{
-    name: String,
-    trappings: String,
-    cost: String,
-    range: String,
-    damage: String,
-    duration: String
-  }],
-  weapons: [{
-    name: String,
-    range: String,
-    rof: String,
-    damage: String,
-    ap: String,
-    weight: Number,
-    notes: String
-  }],
-  equipment: [{
-    name: String,
-    description: String,
-    weight: Number
-  }]
+	userId: String,
+	system: String,
+	name: String,
+	setting: String
 });
 
 // Export the Mongoose model

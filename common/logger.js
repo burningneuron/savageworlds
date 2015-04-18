324c072ea757735bb620f72b15328fe7b15e95c2
Tom Cannon <tomcannon@MacBook-Pro.local>
2015-04-17 19:00:48 -0500

    removed stupid variable

--- common/logger.js
+++ common/logger.js
@@ -2,16 +2,14 @@ var winston = require('winston');
 
 // Instantiate and configure winston logger
 var transports = [];
-if (useConsoleLogger) {
-  transports.push(new winston.transports.Console({
-    level: "debug",
-    handleExceptions: true,
-    json: false,
-    colorize: true,
-    silent: false,
-    timestamp: true
-  }));
-}
+transports.push(new winston.transports.Console({
+  level: "debug",
+  handleExceptions: true,
+  json: false,
+  colorize: true,
+  silent: false,
+  timestamp: true
+}));
 
 var logger = new winston.Logger({
   transports: transports,

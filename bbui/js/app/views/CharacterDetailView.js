2f445d6577ec361857830ac415dc382bc8a8ef89
Tom Cannon <tomcannon@MacBook-Pro.local>
2015-04-24 22:51:31 -0500

    model.toJSON() is essential

--- bbui/js/app/views/CharacterDetailView.js
+++ bbui/js/app/views/CharacterDetailView.js
@@ -1,130 +1,130 @@
 // View.js
 // -------
 define(["jquery",
-    "backbone",
-    "models/Character",
-    "text!templates/characterDetailView.html",
-    "views/SkillListView",
-    "views/EdgeListView",
-    "views/HindranceListView",
-    "views/WeaponListView",
-    "views/PowerListView",
-    "views/EquipmentListView",
-    "views/AdvancementView"
-  ],
+		"backbone",
+		"models/Character",
+		"text!templates/characterDetailView.html",
+		"views/SkillListView",
+		"views/EdgeListView",
+		"views/HindranceListView",
+		"views/WeaponListView",
+		"views/PowerListView",
+		"views/EquipmentListView",
+		"views/AdvancementView"
+	],
 
-  function($, Backbone, Character, template,
-    SkillListView, EdgeListView, HindranceListView, WeaponListView,
-    PowerListView,
-    EquipmentListView, AdvancementView) {
+	function($, Backbone, Character, template,
+		SkillListView, EdgeListView, HindranceListView, WeaponListView,
+		PowerListView,
+		EquipmentListView, AdvancementView) {
 
-    var View = Backbone.View.extend({
-      // View constructor
-      initialize: function(options) {
-        this.router = options.router;
-        this.subviews = [];
+		var View = Backbone.View.extend({
+			// View constructor
+			initialize: function(options) {
+				this.router = options.router;
+				this.subviews = [];
 
-        this.addSubView(SkillListView, 'skills');
-        this.addSubView(EquipmentListView, 'equipment');
-        this.addSubView(EdgeListView, 'edges');
-        this.addSubView(HindranceListView, 'hindrances');
-        this.addSubView(WeaponListView, 'weapons');
-        this.addSubView(PowerListView, 'powers');
-        this.addSubView(AdvancementView, 'advancement', true);
+				this.addSubView(SkillListView, 'skills');
+				this.addSubView(EquipmentListView, 'equipment');
+				this.addSubView(EdgeListView, 'edges');
+				this.addSubView(HindranceListView, 'hindrances');
+				this.addSubView(WeaponListView, 'weapons');
+				this.addSubView(PowerListView, 'powers');
+				this.addSubView(AdvancementView, 'advancement', true);
 
-        this.listenTo(this.model, 'change', this.render);
-      },
+				this.listenTo(this.model, 'change', this.render);
+			},
 
-      addSubView: function(ViewConstructor, property, useModel) {
-        if (useModel) {
-          this.subviews.push({
-            view: new ViewConstructor({
-              model: this.model.get("gameStuff." + property)
-            }),
-            anchor: "#" + property
-          });
-        } else {
-          this.subviews.push({
-            view: new ViewConstructor({
-              collection: this.model.get("gameStuff." +
-                property)
-            }),
-            anchor: "#" + property
-          });
-        }
-      },
+			addSubView: function(ViewConstructor, property, useModel) {
+				if (useModel) {
+					this.subviews.push({
+						view: new ViewConstructor({
+							model: this.model.get("gameStuff." + property)
+						}),
+						anchor: "#" + property
+					});
+				} else {
+					this.subviews.push({
+						view: new ViewConstructor({
+							collection: this.model.get("gameStuff." +
+								property)
+						}),
+						anchor: "#" + property
+					});
+				}
+			},
 
-      close: function() {
-        this.subviews.forEach(function(item) {
-          item.view.close();
-        }.bind(this));
-        this.remove();
-        this.unbind();
-      },
+			close: function() {
+				this.subviews.forEach(function(item) {
+					item.view.close();
+				}.bind(this));
+				this.remove();
+				this.unbind();
+			},
 
-      // View Event Handlers
-      events: {
-        "click #characterList": "gotoCharacterList",
-        "change input.base": "updateModel"
+			// View Event Handlers
+			events: {
+				"click #characterList": "gotoCharacterList",
+				"change input.base": "updateModel"
 
-      },
+			},
 
-      updateModel: function() {
-        this.model.set('setting', this.$("[name='setting']").val());
-        this.model.set('player', this.$("[name='player']").val());
-        this.model.set('name', this.$("[name='name']").val());
+			updateModel: function() {
+				this.model.set('setting', this.$("[name='setting']").val());
+				this.model.set('player', this.$("[name='player']").val());
+				this.model.set('name', this.$("[name='name']").val());
 
-        this.model.set('gameStuff.race', this.$("[name='race']").val());
-        this.model.set('gameStuff.rank', this.$("[name='rank']").val());
-        this.model.set('gameStuff.xp', this.$("[name='xp']").val());
-        this.model.set('gameStuff.money', this.$("[name='money']").val());
+				this.model.set('gameStuff.race', this.$("[name='race']").val());
+				this.model.set('gameStuff.rank', this.$("[name='rank']").val());
+				this.model.set('gameStuff.xp', this.$("[name='xp']").val());
+				this.model.set('gameStuff.money', this.$("[name='money']").val());
 
-        this.model.set('gameStuff.agility', this.$("[name='agility']").val());
-        this.model.set('gameStuff.smarts', this.$("[name='smarts']").val());
-        this.model.set('gameStuff.spirit', this.$("[name='spirit']").val());
-        this.model.set('gameStuff.strength', this.$("[name='strength']")
-          .val());
-        this.model.set('gameStuff.vigor', this.$("[name='vigor']").val());
+				this.model.set('gameStuff.agility', this.$("[name='agility']").val());
+				this.model.set('gameStuff.smarts', this.$("[name='smarts']").val());
+				this.model.set('gameStuff.spirit', this.$("[name='spirit']").val());
+				this.model.set('gameStuff.strength', this.$("[name='strength']")
+					.val());
+				this.model.set('gameStuff.vigor', this.$("[name='vigor']").val());
 
-        this.model.set('gameStuff.charisma', this.$("[name='charisma']")
-          .val());
-        this.model.set('gameStuff.pace', this.$("[name='pace']").val());
-        this.model.set('gameStuff.parry', this.$("[name='parry']").val());
-        this.model.set('gameStuff.toughness', this.$(
-          "[name='toughness']").val());
-      },
+				this.model.set('gameStuff.charisma', this.$("[name='charisma']")
+					.val());
+				this.model.set('gameStuff.pace', this.$("[name='pace']").val());
+				this.model.set('gameStuff.parry', this.$("[name='parry']").val());
+				this.model.set('gameStuff.toughness', this.$(
+					"[name='toughness']").val());
+			},
 
-      gotoCharacterList: function() {
-        this.model.save({
-          success: function() {
-            console.log('model save successful');
-          }.bind(this),
-          failure: function() {
-            console.log('model save failed');
-            // this.router.navigate("", true);
-          }.bind(this)
-        });
-        this.router.navigate("", true);
-      },
+			gotoCharacterList: function() {
+				this.model.save({
+					success: function() {
+						console.log('model save successful');
+					}.bind(this),
+					failure: function() {
+						console.log('model save failed');
+						// this.router.navigate("", true);
+					}.bind(this)
+				});
+				this.router.navigate("", true);
+			},
 
-      // Renders the view's template to the UI
-      render: function() {
-        this.$el.html(_.template(template, this.model.attributes));
+			// Renders the view's template to the UI
+			render: function() {
+				this.$el.html(_.template(template, this.model.toJSON()));
 
-        this.subviews.forEach(function(item) {
-          item.view.$el = this.$(item.anchor);
-          item.view.render();
-          item.view.delegateEvents();
-        }.bind(this));
+				this.subviews.forEach(function(item) {
+					item.view.$el = this.$(item.anchor);
+					item.view.render();
+					item.view.delegateEvents();
+				}.bind(this));
 
-        return this;
-      }
+				return this;
+			}
 
-    });
+		});
 
-    // Returns the View class
-    return View;
+		// Returns the View class
+		return View;
 
-  }
+	}
 
 );

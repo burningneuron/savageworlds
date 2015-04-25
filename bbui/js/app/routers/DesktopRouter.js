ea5a9d8edea49ba8e9599085e0c7df0452ef0652
Tom Cannon <tomcannon@MacBook-Pro.local>
2015-04-24 21:04:49 -0500

    formatting

--- bbui/js/app/routers/DesktopRouter.js
+++ bbui/js/app/routers/DesktopRouter.js
@@ -1,92 +1,97 @@
 // DesktopRouter.js
 // ----------------
 define(["jquery",
-    "backbone",
-    "collections/CharacterCollection",
-    "models/User",
-    "models/Character",
-    "views/CharacterDetailView",
-    "views/CharacterListView",
-    "views/LoginView",
-    "views/ProfileView",
-    "Backbone.Marionette",
-  ],
+		"backbone",
+		"collections/CharacterCollection",
+		"models/User",
+		"models/Character",
+		"views/CharacterDetailView",
+		"views/CharacterListView",
+		"views/LoginView",
+		"views/ProfileView",
+		"Backbone.Marionette",
+	],
 
-  function($, Backbone, CharacterCollection, UserModel, CharacterModel,
-    CharacterDetailView, CharacterListView, LoginView, ProfileView, Marionette) {
+	function($, Backbone, CharacterCollection, UserModel, CharacterModel,
+		CharacterDetailView, CharacterListView, LoginView, ProfileView, Marionette) {
 
-    var DesktopRouter = Backbone.Router.extend({
+		var DesktopRouter = Backbone.Router.extend({
 
-      initialize: function() {
+			initialize: function() {
 
-        this.user = new UserModel();
-        this.user.fetch();
+				this.user = new UserModel();
+				this.user.fetch();
 
-        // Instantiates a new view which will render the header text to the page
-        this.loginView = new LoginView({
-          router: this,
-          model: this.user
-        });
+				// Instantiates a new view which will render the header text to the page
+				this.loginView = new LoginView({
+					router: this,
+					model: this.user
+				});
 
-        // Tells Backbone to start watching for hashchange events
-        Backbone.history.start();
-      },
+				// Tells Backbone to start watching for hashchange events
+				Backbone.history.start();
+			},
 
-      // All of your Backbone Routes (add more)
-      routes: {
+			// All of your Backbone Routes (add more)
+			routes: {
 
-        // When there is no hash on the url, the home method is called
-        "": "index",
-        "character?id=:id": "character",
-        "profile": "profile"
+				// When there is no hash on the url, the home method is called
+				"": "index",
+				"character?id=:id": "character",
+				"profile": "profile"
 
-      },
+			},
 
-      showView: function(view) {
-        if (this.currentView) {
-          this.currentView.close ? this.currentView.close() : this.currentView.remove();
-        }
+			showView: function(view) {
+				if (this.currentView && this.currentView.close) {
+					this.currentView.close();
+				} else if (this.currentView) {
+					this.currentView.remove();
+				}
 
-        this.currentView = view;
-        $("#pageContent").html(this.currentView.el);
-        this.currentView.render();
+				this.currentView = view;
+				$("#pageContent").html(this.currentView.el);
+				this.currentView.render();
 
-      },
+			},
 
-      index: function() {
-        var characters = new CharacterCollection();
-        characters.fetch();
+			index: function() {
+				var characters = new CharacterCollection();
+				characters.fetch();
 
-        this.showView(new CharacterListView({
-          router: this,
-          collection: characters
-        }));
-      },
+				this.showView(new CharacterListView({
+					router: this,
+					collection: characters
+				}));
+			},
 
-      profile: function() {
-        this.showView(new ProfileView({
-          router: this,
-          model: this.user
-        }));
-      },
+			profile: function() {
+				this.showView(new ProfileView({
+					router: this,
+					model: this.user
+				}));
+			},
 
-      character: function(id) {
-        var character = new CharacterModel({
-          _id: id
-        });
-        character.fetch();
+			character: function(id) {
+				var character = new CharacterModel({
+					_id: id
+				});
+				character.fetch({
+					success: function(model, response, callback) {
+						this.showView(new CharacterDetailView({
+							router: this,
+							model: model
+						}));
+					}.bind(this)
+				});
 
-        this.showView(new CharacterDetailView({
-          router: this,
-          model: character
-        }));
-      }
+			}
 
-    });
+		});
 
-    // Returns the DesktopRouter class
-    return DesktopRouter;
+		// Returns the DesktopRouter class
+		return DesktopRouter;
 
-  }
+	}
 
 );
--- bbui/js/app/views/CharacterDetailView.js
+++ bbui/js/app/views/CharacterDetailView.js
@@ -1,122 +1,120 @@
 // View.js
 // -------
 define(["jquery",
-    "backbone",
-    "models/Character",
-    "text!templates/characterDetailView.html",
-    "views/SkillListView",
-    "views/EdgeListView",
-    "views/HindranceListView",
-    "views/WeaponListView",
-    "views/PowerListView",
-    "views/EquipmentListView",
-    "views/AdvancementView"
-  ],
+		"backbone",
+		"models/Character",
+		"text!templates/characterDetailView.html",
+		"views/SkillListView",
+		"views/EdgeListView",
+		"views/HindranceListView",
+		"views/WeaponListView",
+		"views/PowerListView",
+		"views/EquipmentListView",
+		"views/AdvancementView"
+	],
 
-  function($, Backbone, Character, template,
-    SkillListView, EdgeListView, HindranceListView, WeaponListView, PowerListView,
-    EquipmentListView, AdvancementView) {
+	function($, Backbone, Character, template,
+		SkillListView, EdgeListView, HindranceListView, WeaponListView,
+		PowerListView,
+		EquipmentListView, AdvancementView) {
 
-    var View = Backbone.View.extend({
-      // View constructor
-      initialize: function(options) {
-        this.router = options.router;
-        // Calls the view's render method
-        this.listenTo(this.model, 'change', this.render);
+		var View = Backbone.View.extend({
+			// View constructor
+			initialize: function(options) {
+				this.router = options.router;
+				this.subviews = [];
 
-        // this.backgroundView = new CharacterBackgroundView({
-        //   model: this.model
-        // });
+				this.addSubView(SkillListView, 'skills');
+				this.addSubView(EquipmentListView, 'equipment');
+				this.addSubView(EdgeListView, 'edges');
+				this.addSubView(HindranceListView, 'hindrances');
+				this.addSubView(WeaponListView, 'weapons');
+				this.addSubView(PowerListView, 'powers');
+				this.addSubView(AdvancementView, 'advancement', true);
 
-        this.subviews = [];
+				this.listenTo(this.model, 'change', this.render);
+			},
 
-        this.addSubView(SkillListView, 'skills');
-        this.addSubView(EquipmentListView, 'equipment');
-        this.addSubView(EdgeListView, 'edges');
-        this.addSubView(HindranceListView, 'hindrances');
-        this.addSubView(WeaponListView, 'weapons');
-        this.addSubView(PowerListView, 'powers');
-        this.addSubView(AdvancementView, 'advancement', true);
+			addSubView: function(ViewConstructor, property, useModel) {
+				if (useModel) {
+					this.subviews.push({
+						view: new ViewConstructor({
+							model: this.model.get(property)
+						}),
+						anchor: "#" + property
+					});
+				} else {
+					this.subviews.push({
+						view: new ViewConstructor({
+							collection: this.model.get(property)
+						}),
+						anchor: "#" + property
+					});
+				}
+			},
 
-      },
+			close: function() {
+				this.subviews.forEach(function(item) {
+					item.view.close();
+				}.bind(this));
+				this.remove();
+				this.unbind();
+			},
 
-      addSubView: function(ViewConstructor, property, useModel) {
-        if (useModel) {
-          this.subviews.push({
-            view: new ViewConstructor({
-              model: this.model.get(property)
-            }),
-            anchor: "#" + property
-          });
-        } else {
-          this.subviews.push({
-            view: new ViewConstructor({
-              collection: this.model.get(property)
-            }),
-            anchor: "#" + property
-          });
-        }
-      },
+			// View Event Handlers
+			events: {
+				"click #characterList": "gotoCharacterList",
+				"change input.base": "updateModel"
 
-      close: function() {
-        this.subviews.forEach(function(item) {
-          item.view.close();
-        }.bind(this));
-        this.remove();
-        this.unbind();
-      },
+			},
 
-      // View Event Handlers
-      events: {
-        "click #characterList": "gotoCharacterList",
-        "change input.base": "updateModel"
+			updateModel: function() {
+				this.model.set('setting', this.$("[name='setting']").val());
+				this.model.set('player', this.$("[name='player']").val());
+				this.model.set('name', this.$("[name='name']").val());
+				this.model.set('race', this.$("[name='race']").val());
+				this.model.set('rank', this.$("[name='rank']").val());
+				this.model.set('xp', this.$("[name='xp']").val());
+				this.model.set('money', this.$("[name='money']").val());
 
-      },
+				this.model.set('agility', this.$("[name='agility']").val());
+				this.model.set('smarts', this.$("[name='smarts']").val());
+				this.model.set('spirit', this.$("[name='spirit']").val());
+				this.model.set('strength', this.$("[name='strength']").val());
+				this.model.set('vigor', this.$("[name='vigor']").val());
 
-      updateModel: function() {
-        this.model.set('setting', this.$("[name='setting']").val());
-        this.model.set('player', this.$("[name='player']").val());
-        this.model.set('name', this.$("[name='name']").val());
-        this.model.set('race', this.$("[name='race']").val());
-        this.model.set('rank', this.$("[name='rank']").val());
-        this.model.set('xp', this.$("[name='xp']").val());
-        this.model.set('money', this.$("[name='money']").val());
+				this.model.set('charisma', this.$("[name='charisma']").val());
+				this.model.set('pace', this.$("[name='pace']").val());
+				this.model.set('parry', this.$("[name='parry']").val());
+				this.model.set('toughness', this.$("[name='toughness']").val());
+			},
 
-        this.model.set('agility', this.$("[name='agility']").val());
-        this.model.set('smarts', this.$("[name='smarts']").val());
-        this.model.set('spirit', this.$("[name='spirit']").val());
-        this.model.set('strength', this.$("[name='strength']").val());
-        this.model.set('vigor', this.$("[name='vigor']").val());
+			gotoCharacterList: function() {
+				this.model.save({
+					success: function() {
+						this.router.navigate("", true);
+					}
+				});
+			},
 
-        this.model.set('charisma', this.$("[name='charisma']").val());
-        this.model.set('pace', this.$("[name='pace']").val());
-        this.model.set('parry', this.$("[name='parry']").val());
-        this.model.set('toughness', this.$("[name='toughness']").val());
-      },
+			// Renders the view's template to the UI
+			render: function() {
+				this.$el.html(_.template(template, this.model.attributes));
 
-      gotoCharacterList: function() {
-        console.log(this.model.toJSON());
-        // this.router.navigate("", true);
-      },
+				this.subviews.forEach(function(item) {
+					item.view.$el = this.$(item.anchor);
+					item.view.render();
+					item.view.delegateEvents();
+				}.bind(this));
 
-      // Renders the view's template to the UI
-      render: function() {
-        this.$el.html(_.template(template, this.model.attributes));
+				return this;
+			}
 
-        this.subviews.forEach(function(item) {
-          item.view.$el = this.$(item.anchor);
-          item.view.render();
-          item.view.delegateEvents();
-        }.bind(this));
+		});
 
-        return this;
-      }
+		// Returns the View class
+		return View;
 
-    });
-
-    // Returns the View class
-    return View;
-
-  }
+	}
 
 );

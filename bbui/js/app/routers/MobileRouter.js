62769eb8b6c5f80790c83947ffa358a10b1c392b
Tom Cannon <tom.cannon@connectsolutions.com>
2015-04-27 15:11:09 -0500

    gonna just punch out the mobile router and see if it does anything

--- bbui/js/app/routers/MobileRouter.js
+++ bbui/js/app/routers/MobileRouter.js
@@ -1,38 +1,133 @@
 // MobileRouter.js
-// ---------------
-define(["jquery", "backbone", "models/Model", "views/View", "collections/Collection"],
-        
-    function($, Backbone, UserModel, View, Collection) {
+// ----------------
+define(["jquery",
+		"backbone",
+		"collections/CharacterCollection",
+		"models/User",
+		"models/Character",
+		"views/CharacterListView",
+		"views/LoginView",
+		"views/ProfileView",
+		"views/AboutView",
+		"Backbone.Marionette",
+		"savageworlds/models/Character",
+		"savageworlds/display/views/CharacterDetailView",
+		"savageworlds/edit/views/CharacterDetailView",
+	],
 
-        var MobileRouter = Backbone.Router.extend({
+	function($, Backbone, CharacterCollection, UserModel, CharacterModel, CharacterListView,
+		LoginView, ProfileView, AboutView, Marionette, SavageWorldsCharacterModel,
+			SavageWorldsCharacterDisplay, SavageWorldsCharacterEditor) {
 
-            initialize: function() {
+		var MobileRouter = Backbone.Router.extend({
 
-                // Tells Backbone to start watching for hashchange events
-                Backbone.history.start();
+			initialize: function() {
 
-            },
+				this.user = new UserModel();
+				this.user.fetch();
 
-            // All of your Backbone Routes (add more)
-            routes: {
-                
-                // When there is no hash bang on the url, the home method is called
-                "": "index"
+				// Instantiates a new view which will render the header text to the page
+				this.loginView = new LoginView({
+					router: this,
+					model: this.user
+				});
 
-            },
+				// Tells Backbone to start watching for hashchange events
+				Backbone.history.start();
+			},
 
-            index: function() {
+			// All of your Backbone Routes (add more)
+			routes: {
 
-                // Instantiates a new view which will render the header text to the page
-                new View();
+				// When there is no hash on the url, the home method is called
+				"": "index",
+        "addCharacter": "addCharacter",
+				"characters": "listCharacters",
+				"editCharacter?id=:id": "editCharacter",
+				"showCharacter?id=:id": "showCharacter",
+				"profile": "profile",
 
-            }
-    
-        });
+			},
 
-        // Returns the MobileRouter class
-        return MobileRouter;
+			showView: function(view) {
+				if (this.currentView && this.currentView.close) {
+					this.currentView.close();
+				} else if (this.currentView) {
+					this.currentView.remove();
+				}
 
-    }
+				this.currentView = view;
+				$("#pageContent").html(this.currentView.el);
+				this.currentView.render();
+
+			},
+
+			index: function() {
+				this.showView(new AboutView({
+					router: this
+				}));
+			},
+
+			listCharacters: function() {
+				var characters = new CharacterCollection();
+				characters.fetch();
+
+				this.showView(new CharacterListView({
+					router: this,
+					collection: characters
+				}));
+			},
+
+			profile: function() {
+				this.showView(new ProfileView({
+					router: this,
+					model: this.user
+				}));
+      },
+
+			addCharacter: function() {
+        var character = new SavageWorldsCharacterModel();
+				// seems necessary to ensure defaults are fully constructed when view is created
+				character.set(CharacterModel.defaults);
+
+        this.showView(new SavageWorldsCharacterEditor({
+          router: this,
+          model: character
+        }));
+			},
+
+			editCharacter: function(id) {
+				var character = new SavageWorldsCharacterModel({
+					_id: id
+				});
+				character.fetch({
+					success: function(model, response, callback) {
+						this.showView(new SavageWorldsCharacterEditor({
+							router: this,
+							model: model
+						}));
+					}.bind(this)
+				});
+			},
+
+			showCharacter: function(id) {
+				var character = new SavageWorldsCharacterModel({
+					_id: id
+				});
+				character.fetch({
+					success: function(model, response, callback) {
+						this.showView(new SavageWorldsCharacterDisplay({
+							router: this,
+							model: model
+						}));
+					}.bind(this)
+				});
+			}
+		});
+
+		// Returns the MobileRouter class
+		return MobileRouter;
+
+	}
 
 );
